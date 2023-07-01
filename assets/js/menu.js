7232fb27c253dfdbef262c32377c3a074d0f59c3
LZ7-owo <2097696082@qq.com>
2023-07-01 22:22:55 +0800

    menu

---
+++ assets/js/menu.js
@@ -0,0 +1,8 @@
+function setTchMenu() {
+	// 标题内容
+    document.getElementById("menu_title").innerHTML   = 
+	"<i class=\"fas fa-leaf\"></i>导航"
+	// 子条目内容
+    document.getElementById("menu_content").innerHTML   = 
+	"<li><a href=index.html>图形学</a></li><li><a href=>艺术设计</a></li><li><a href=>编程</a></li><li><a href=>关于</a></li>"
+}
